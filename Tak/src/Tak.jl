855d3651cc661c8a8e910e4b77b13f7cc37e1a27
Pietro Jomini <pietro.jomini@protonmail.ch>
2023-05-16 11:43:53 +0200

    placements

--- Tak/src/Tak.jl
+++ Tak/src/Tak.jl
@@ -2,6 +2,7 @@ module Tak
 
 include("bitboard.jl")
 include("position.jl")
+include("move.jl")
 include("tps.jl")
 
 end
--- Tak/src/bitboard.jl
+++ Tak/src/bitboard.jl
@@ -11,7 +11,7 @@ union(b1::Bitboard, b2::Bitboard) = Bitboard(b1.raw | b2.raw, max(b1.size, b2.si
 intersect(b1::Bitboard, b2::Bitboard) = Bitboard(b1.raw & b2.raw, max(b1.size, b2.size))
 issubset(b1::Bitboard, b2::Bitboard) = b2 == (b1 ∪ b2)
 in(b1::Bitboard, b2::Bitboard) = b1 ⊆ b2
-~(bb::Bitboard) = Bitboard(~bb.raw, bb.size)
+~(bb::Bitboard) = Bitboard(~bb.raw, bb.size) ∩ universe(bb.size)
 +(b1::Bitboard, b2::Bitboard) = b1 ∪ b2
 +(bb::Bitboard, value::Int) = Bitboard(bb.raw + value, bb.size)
 -(b1::Bitboard, b2::Bitboard) = b1 ∩ ~b2
@@ -29,6 +29,20 @@ empty(size=8) = Bitboard(0, size)
 
 #https://docs.julialang.org/en/v1/manual/interfaces/#man-interfaces-broadcasting
 Base.broadcastable(x::Bitboard) = Ref(Bitboard)
+
+function Base.iterate(bb::Bitboard, state=1)
+    if state > bb.size^2
+        return nothing
+    end
+
+    for index in state:bb.size^2
+        s = square(index, bb.size)
+        if s ∈ bb return (s, index + 1) end
+    end
+end
+
+Base.eltype(::Type{Bitboard}) = Bitboard
+Base.length(bb::Bitboard) = count_ones(bb.raw)
 
 function Base.show(io::IO, bb::Bitboard)
     println("Bitboard{$(bb.size)x$(bb.size)}(0x$(string(bb.raw, base=16, pad=16))):")
