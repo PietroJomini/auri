56e927715afb3c33a498df1f79843344cd7b6169
Pietro Jomini <pietro.jomini@protonmail.ch>
2023-05-20 13:59:49 +0200

    moves still bad, but at least right

--- tak/src/move.jl
+++ tak/src/move.jl
@@ -29,22 +29,17 @@ struct Slide <: Move
     height::SVector{8,UInt8}
 end
 
-function compute_slides(height::Int,  cap::Int, previous::Vector{Int} = Vector{Int}())
-    if height == 0 || length(previous) == cap
-        return [previous]
-    # ensure that the slide will always consume all the stones
-    elseif height == 1 || length(previous) == cap - 1
-        return [[previous; height]]
-    else
-        min_drop = min(1, length(previous))
-        return [((compute_slides(height - dropped, cap, [previous; dropped]) for dropped in min_drop:height)...)...]
-    end
+function compute_slides(height::Int, cap::Int, previous::Vector{Int} = Vector{Int}())
+    (height == 0 || length(previous) == cap) && return [previous]
+
+    leafs = map(i -> compute_slides(height - i, cap, [previous; i]), 1:height)
+    return unique([[height], (leafs...)...])
 end
 
 function legal_slides(pos::Position, dir::Direction, at::Square)
     height = min(pos.size, pos.heights[at.index])
 
-    cap = 1
+    cap = 0
     nat = at
     nnat = slide(pos.size, nat, dir)
     while dir ∈ neighbors(pos.size, nat) && nnat ∉ (pos.walls ∪ pos.caps)
@@ -62,8 +57,7 @@ function legal_slides(pos::Position, dir::Direction, at::Square)
         slides =  compute_slides(Int(height), cap)
     end
 
-    # disallow dropping all stones on the origin square
-    return filter(s -> length(s) > 1, slides)
+    return slides
 end
 
 function slides(pos::Position, at::Square)::Vector{Slide}
