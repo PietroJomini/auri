c8ec1e2e09e73c74dd9953a8659c4522741d161a
Pietro Jomini <pietro.jomini@protonmail.ch>
2023-05-21 05:03:02 +0200

    tests placebo

--- tak/src/Tak.jl
+++ tak/src/Tak.jl
@@ -9,4 +9,8 @@ include("moves/perf.jl")
 include("routes.jl")
 include("ptn/Ptn.jl")
 
+# tests placebo
+export hello_tak
+hello_tak(size::BitboardSize) = size == BBS_7 ? "spooky blue flame" : "comfy red candle"
+
 end
--- tak/test/runtests.jl
+++ tak/test/runtests.jl
@@ -4,6 +4,8 @@ using Test
 @testset "assert environment" begin
     @test true
     @test 1 + 1 == 2
+    @test Tak.hello_tak(BBS_5) == "comfy red candle"
+    @test Tak.hello_tak(BBS_7) == "spooky blue flame"
 end
 
 include("perf.jl")
