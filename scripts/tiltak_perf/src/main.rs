99af342e65e4d4560b5f325eb0de788895eec66b
Pietro Jomini <pietro.jomini@protonmail.ch>
2023-05-21 07:34:51 +0200

    still doesn't know what, but it does something

--- scripts/tiltak_perf/src/main.rs
+++ scripts/tiltak_perf/src/main.rs
@@ -1,5 +1,4 @@
-extern crate tiltak;
-
+use std::io::{ self, BufRead };
 use tiltak::position::Position;
 use pgn_traits::PgnPosition;
 
@@ -7,16 +6,20 @@ use pgn_traits::PgnPosition;
 TODOS:
 - actually understand what tiltak's perft does
     - why does the move number changes the result?
-- fetch tpss from file
-- write perf results to csv
+x fetch tpss from file -> cargo run < positions.tps
+x write perf results to csv
 */
 
 fn main() {
-    // let tps = String::from("x5/x5/x5/x5/x5 1 1");
-    let tps = String::from("x3,12,2S/x,22S,22C,11,21/121,212,12,1121C,1212S/21S,1,21,211S,12S/x,21S,2,x2 1 1");
-    // let mut position: Position<5> = Position::default();
+    for line in io::stdin().lock().lines() {
+        perft(line.unwrap())
+    }
+}
 
+fn perft(tps:String) {
+    // TODO: boards with different sizes
     let mut position = <Position<5>>::from_fen(&tps).unwrap();
-
-    println!("{}", position.bulk_perft(1));
+    print!("{}", tps);
+    for i in 1..5 { print!(";{}", position.bulk_perft(i)) }
+    println!();
 }
